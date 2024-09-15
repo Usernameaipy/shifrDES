98ea38e369f5f2fe0549b3b410b3b6e369903cdd
moon <undruee@gmail.com>
2024-09-15 16:29:43 +0300

    files

--- src/des_shifr/des_shifr.h
+++ src/des_shifr/des_shifr.h
@@ -1,6 +1,6 @@
 #ifndef DES_SHIFR_H
 #define DES_SHIFR_H
-#include "input.h"
+#include "../input/input.h"
 #define KEY_DES "idinaxuy"
 void start(void);
 #endif
--- src/test_des_shifr/test_des_shifr.h
+++ src/test_des_shifr/test_des_shifr.h
@@ -1,4 +1,4 @@
 #ifndef TEST_DES_SHIFR_H
 #define TEST_DES_SHIFR_H
-#include "des_shifr.h"
+#include "../des_shifr/des_shifr.h"
 #endif
